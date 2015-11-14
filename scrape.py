db261c304d46176b0d3a01a9fc3fc5a5c2df1a12
Wyatt Cooper <wcooper17@cmc.edu>
2015-11-14 06:04:13 -0800

    Added a lot of functionality

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import smtplib
 import imaplib
 import email
+import re
+from bs4 import BeautifulSoup
 from email.utils import getaddresses
 
 #move emails to new folder once done
@@ -34,10 +36,11 @@ def process_inbox(server):
         #ccs = msg.get_all('cc', [])
         resent_tos = msg.get_all('resent-to', [])
         #resent_ccs = msg.get_all('resent-cc', [])
-
-        all_recipients = getaddresses(tos + resent_tos)
+        froms = msg.get_all('from', [])
+        all_recipients = getaddresses(froms)
         tuple = all_recipients[0]
         incomingAddress = tuple[1]
+        #print incomingAddress
 
         #print "From: " + tuple[1]
         #print "To: " + tuple[0]
@@ -49,15 +52,20 @@ def process_inbox(server):
                 body.append(payload.get_payload())
             #print 'Body: '+ (body[0])
             text = body[0]
-
+            if (type(text) is list):
+                text = text[0].as_string()
+                for item in text.split("</td>\r\n"):
+                    if "<td>\r\n" in item:
+                        print item[item.find("<td>\r\n")+len("</td>\r\n"):]
+                        text = item[item.find("<td>\r\n")+len("</td>\r\n"):]
         else:
-            text = "no text"
+            text = "No text"
             #print msg.get_payload()
 
         #print 'Subject %s: %s' % (num, msg['Subject'])
         subject = msg['Subject']
         #print num
-        #print (subject, incomingAddress, text)
+        print (subject, incomingAddress, text)
         rtn[num] = (subject, incomingAddress, text)
     return rtn
 
@@ -66,30 +74,41 @@ def process_inbox(server):
 def findFromPhone(dictionary):
     setList = []
     deleteList = []
-    print dictionary.keys()
-    for key in range(1,len(dictionary.keys())):
+    #print dictionary.keys()
+    for key in range(1,len(dictionary.keys())+1):
         key = str(key)
+        #print key
+        #print dictionary[key][0]
         #this is the incomingAddress
-        print dictionary[key][1]
         bodyStr = dictionary[key][2]
+        #print bodyStr
         bodyWords = bodyStr.split()
-        print bodyWords[0]
-        if (dictionary[key][1] == '4256475206@txt.att.net'):
+        #print bodyWords[0]
+        print "From:" + dictionary[key][1]
+        if (dictionary[key][1] == '4256475206@mms.att.net'):
+            print "this is from kyle"
             if (bodyWords[0] == "Set"):
+                print "found a set command"
                 setList.append(bodyWords[1])
             elif (bodyWords[0] == "Delete"):
                 deleteList.append(bodyWords[1])
     #print setList
     #print deleteList
     return (setList, deleteList)
-    
 
 
-
+def sendAlertUsingSetList(setlist,dictionary, number):
+    for word in setlist:
+        for key in range(1,len(dictionary.keys())+1):
+            key = str(key)
+            bodyStr = dictionary[key][2]
+            nameOfSender = dictionary[key][1]
+            if word in bodyStr:
+                strToSend = "You got an email about: "+word+"from " + nameOfSender
+                text(number, strToSend)
 
 # Need to put texting and emailing in these functions
 def text (number, content):
-    content = 'did this make it to ur phone'
     mail = smtplib.SMTP('smtp.gmail.com', 587)
     mail.ehlo()
     mail.starttls()
@@ -128,6 +147,8 @@ def main():
     setList = listOfSets[0]
 
     deleteList = listOfSets[1]
+
+    sendAlertUsingSetList(setList, dictOfMail, "4256475206@txt.att.net")
 
     print setList
 
